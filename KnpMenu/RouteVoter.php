b7dbfd3f74f1571a10ba949ec250d7e3914d2f63
Sven Joder <sj@pmdevelopment.de>
2013-08-27 09:58:51 +0200

    knp menu route voter

---
+++ KnpMenu/RouteVoter.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace PM\Bundle\ToolBundle\KnpMenu;
+
+use Knp\Menu\ItemInterface;
+use Knp\Menu\Matcher\Voter\VoterInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\DependencyInjection\ContainerInterface as Container;
+
+/**
+ * Description of RouteVoter
+ *
+ * @author sjoder
+ */
+class RouteVoter implements VoterInterface
+{
+    /**
+     * @var Request
+     */
+    private $request;
+    
+    public function __construct(Container $container) {
+       $request = $container->get('request');
+       
+       $this->setRequest($request);
+    }
+
+    public function setRequest(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    public function matchItem(ItemInterface $item)
+    {
+        if (null === $this->request) {
+            return null;
+        }
+
+        $route = $this->request->attributes->get('_route');
+        if (null === $route) {
+            return null;
+        }
+
+        $routes = (array) $item->getExtra('routes', array());
+        
+        // $parameters = (array) $item->getExtra('routesParameters', array());
+        
+        foreach ($routes as $testedRoute) {
+            if ($route == $testedRoute) {
+                return true;
+            }
+            
+            
+        }
+
+        return null;
+    }
+}
