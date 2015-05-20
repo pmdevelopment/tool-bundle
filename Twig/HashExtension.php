d3f7a6cfd1ebf1bd2dc548dd28f8dc7a13fc54c3
Sven Joder <sj@pmdevelopment.de>
2015-05-20 16:16:26 +0200

    extending hash twig functions

--- Twig/HashExtension.php
+++ Twig/HashExtension.php
@@ -4,36 +4,87 @@ namespace PM\Bundle\ToolBundle\Twig;
 
 use Twig_Extension;
 use Twig_Filter_Method;
+
 /**
  * Description of CssVersionExtension
  *
  * @author sjoder
  */
-class HashExtension extends Twig_Extension {
+class HashExtension extends Twig_Extension
+{
 
-   public function getFilters() {
-      return array(
-          'sha1' => new Twig_Filter_Method($this, 'hashSHA1'),
-          'md5' => new Twig_Filter_Method($this, 'hashMD5'),
-      );
-   }
+    /**
+     * @return array
+     */
+    public function getFilters()
+    {
+        return array(
+            'hash'  => new Twig_Filter_Method($this, 'hash'),
+            'sha1'  => new Twig_Filter_Method($this, 'hashSHA1'),
+            'md5'   => new Twig_Filter_Method($this, 'hashMD5'),
+            'crc32' => new Twig_Filter_Method($this, 'hashCRC'),
+        );
+    }
 
-   public function hashSHA1($string){
-      if(empty($string))
-         return sha1(microtime());
-      
-      return sha1($string);
-   }
-   
-   public function hashMD5($string){
-      if(empty($string))
-         return md5(microtime());
-      
-      return md5($string);
-   }
+    /**
+     * @param string $string
+     * @param string $algo
+     *
+     * @return string
+     */
+    public function hash($string, $algo)
+    {
+        if (true === empty($string)) {
+            $string = microtime() . uniqid();
+        }
 
-   public function getName() {
-      return 'plugin_pm_hash';
-   }
+        return hash($algo, $string);
+    }
+
+    /**
+     * Get SHA1
+     *
+     * @param string $string
+     *
+     * @return string
+     */
+    public function hashSHA1($string)
+    {
+        return $this->hash($string, "sha1");
+    }
+
+    /**
+     * Get MD5
+     *
+     * @param string $string
+     *
+     * @return string
+     */
+    public function hashMD5($string)
+    {
+        return $this->hash($string, "md5");
+    }
+
+    /**
+     * Get CRC32
+     *
+     * @param string $string
+     *
+     * @return int
+     */
+    public function hashCRC($string)
+    {
+        return crc32($string);
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return 'plugin_pm_hash';
+    }
 
 }
