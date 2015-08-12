404eae1511a320a4bc895bdcc1121e9ba89710fc
Sven Joder <sj@pmdevelopment.de>
2015-08-12 15:34:54 +0200

    bugfix: cssversion by existing file

--- Twig/CssVersionExtension.php
+++ Twig/CssVersionExtension.php
@@ -9,25 +9,63 @@ use Twig_Extension;
  *
  * @author sjoder
  */
-class CssVersionExtension extends Twig_Extension {
+class CssVersionExtension extends Twig_Extension
+{
 
-   public function getFilters() {
-      return array(
-          'cssversion' => new \Twig_Filter_Method($this, 'cssVersion'),
-      );
-   }
+    /**
+     * @var string
+     */
+    private $kernelRootDir;
 
-   public function cssVersion($file) {
-      if (file_exists($file))
-         $string = "$file?v=" . substr(md5_file($file), 0, 5);
-      else
-         return "$file?v=" . substr(sha1(microtime()), 0, 6);
+    /**
+     * CssVersionExtension constructor.
+     *
+     * @param string $kernelRootDir
+     */
+    public function __construct($kernelRootDir)
+    {
+        $this->kernelRootDir = $kernelRootDir;
+    }
 
-      return $string;
-   }
 
-   public function getName() {
-      return 'plugin_pm_cssversion';
-   }
+    /**
+     * Get Filters
+     *
+     * @return array
+     */
+    public function getFilters()
+    {
+        return array(
+            'cssversion' => new \Twig_Filter_Method($this, 'cssVersion'),
+        );
+    }
+
+    /**
+     * CSS Version
+     *
+     * @param string $fileName
+     *
+     * @return string
+     */
+    public function cssVersion($fileName)
+    {
+        $filePath = sprintf("%s/../web%s", $this->kernelRootDir, $fileName);
+
+        if (file_exists($filePath)) {
+            return "$fileName?v=" . substr(md5_file($filePath), 0, 5);
+        }
+
+        return "$fileName?v=" . substr(sha1(microtime()), 0, 6);
+    }
+
+    /**
+     * Get Name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return 'plugin_pm_cssversion';
+    }
 
 }
