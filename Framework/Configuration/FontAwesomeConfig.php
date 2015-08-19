a921acb57363f0e266658013cef7f22aa0244b3d
Sven Joder <sj@pmdevelopment.de>
2015-08-19 12:17:10 +0200

    Fontawesome 4.4.0, utility to get icon classes

---
+++ Framework/Configuration/FontAwesomeConfig.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sjoder
+ * Date: 19.08.15
+ * Time: 11:37
+ */
+
+namespace PM\Bundle\ToolBundle\Framework\Configuration;
+
+/**
+ * Class FontAwesomeConfig
+ *
+ * @package PM\Bundle\ToolBundle\Framework\Configuration
+ */
+class FontAwesomeConfig
+{
+
+    /**
+     * Get Versions available for this config
+     *
+     * @return array
+     */
+    public static function getVersions()
+    {
+        return array(
+            "4.4.0"
+        );
+    }
+
+    /**
+     * Get Icon Classes
+     *
+     * @param string $version
+     *
+     * @return array
+     */
+    public static function getClasses($version)
+    {
+        if (false === in_array($version, self::getVersions())) {
+            throw new \LogicException(sprintf("Version is not supported. Available: %s", implode(", ", self::getVersions())));
+        }
+
+        $version = explode(".", $version);
+        $subVersion = $version[1];
+
+        $icons = array();
+
+        /**
+         * 4.4.0 Icons
+         */
+        if (4 <= $subVersion) {
+            $icons = FontAwesome\Version440::getIconsAll();
+        }
+
+        return $icons;
+    }
+
+}
