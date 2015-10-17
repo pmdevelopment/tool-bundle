3dd8032efbb7311b17573ef8382d5fb3f86ff3fa
Sven <sj@pmdevelopment.de>
2015-10-17 14:23:49 +0200

    Fixing Crypt Key length

--- Framework/Utilities/CryptUtility.php
+++ Framework/Utilities/CryptUtility.php
@@ -20,16 +20,22 @@ class CryptUtility
      *
      * @param string $sValue
      * @param string $sSecretKey
+     * @param bool   $migration Use old buggy key or not
      *
      * @return string
      */
-    public static function encrypt($sValue, $sSecretKey)
+    public static function encrypt($sValue, $sSecretKey, $migration = false)
     {
         if (empty($sValue)) {
             return "";
         }
 
-        $sSecretKey = substr($sSecretKey, 0, 30);
+        if (true === $migration) {
+            $sSecretKey = substr($sSecretKey, 0, 30) . "\0\0";
+        } else {
+            $sSecretKey = substr($sSecretKey, 0, 32);
+        }
+
 
         return rtrim(base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $sSecretKey, $sValue, MCRYPT_MODE_ECB, mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB), MCRYPT_RAND))), "\0");
     }
@@ -39,16 +45,21 @@ class CryptUtility
      *
      * @param string $sValue
      * @param string $sSecretKey
+     * @param bool   $migration Use old buggy key or not
      *
      * @return string
      */
-    public static function decrypt($sValue, $sSecretKey)
+    public static function decrypt($sValue, $sSecretKey, $migration = false)
     {
         if (empty($sValue)) {
             return "";
         }
 
-        $sSecretKey = substr($sSecretKey, 0, 30);
+        if (true === $migration) {
+            $sSecretKey = substr($sSecretKey, 0, 30) . "\0\0";
+        } else {
+            $sSecretKey = substr($sSecretKey, 0, 32);
+        }
 
         return rtrim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $sSecretKey, base64_decode($sValue), MCRYPT_MODE_ECB, mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB), MCRYPT_RAND)), "\0");
     }
