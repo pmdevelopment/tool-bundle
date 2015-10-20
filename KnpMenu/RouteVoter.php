06d73cf1379946b509a7c8cf8c79e103cd11c075
Sven Joder <sj@pmdevelopment.de>
2015-10-20 17:45:13 +0200

    bugfix: undo route voter. use workaround

--- KnpMenu/RouteVoter.php
+++ KnpMenu/RouteVoter.php
@@ -4,73 +4,88 @@ namespace PM\Bundle\ToolBundle\KnpMenu;
 
 use Knp\Menu\ItemInterface;
 use Knp\Menu\Matcher\Voter\VoterInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\DependencyInjection\ContainerInterface as Container;
 
 /**
  * Description of RouteVoter
  *
  * @author sjoder
  */
-class RouteVoter implements VoterInterface {
+class RouteVoter implements VoterInterface
+{
 
-   /**
-    * @var Request
-    */
-   private $request;
+    /**
+     * @var Request
+     */
+    private $request;
 
-   public function __construct(Container $container) {
-      $request = $container->get('request');
+    /**
+     * Constructor
+     *
+     * @param $request
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        $this->request = $container->get("request");
+    }
 
-      $this->setRequest($request);
-   }
+    /**
+     * @return Request
+     */
+    public function getRequest()
+    {
+        return $this->request;
+    }
 
-   public function setRequest(Request $request) {
-      $this->request = $request;
-   }
+    /**
+     * @param ItemInterface $item
+     *
+     * @return bool|null
+     */
+    public function matchItem(ItemInterface $item)
+    {
+        if (null === $this->getRequest()) {
+            return null;
+        }
 
-   public function matchItem(ItemInterface $item) {
-      if (null === $this->request) {
-         return null;
-      }
+        $route = $this->getRequest()->attributes->get('_route');
+        $routeParameters = $this->getRequest()->attributes->get('_route_params');
 
-      $route = $this->request->attributes->get('_route');
-      $routeParameters = $this->request->attributes->get('_route_params');
-
-      if (null === $route) {
-         return null;
-      }
+        if (null === $route) {
+            return null;
+        }
 
 
-      $routes = (array) $item->getExtra('routes', array());
+        $routes = (array)$item->getExtra('routes', array());
 
 
-      foreach ($routes as $testedRoute) {
-         if ($route == $testedRoute['route']) {
+        foreach ($routes as $testedRoute) {
+            if ($route == $testedRoute['route']) {
 
-            if (is_array($routeParameters) && 0 < count($routeParameters)) {
-               if (isset($testedRoute['parameters']) && is_array($testedRoute['parameters'])) {
-                  $matching = true;
+                if (is_array($routeParameters) && 0 < count($routeParameters)) {
+                    if (isset($testedRoute['parameters']) && is_array($testedRoute['parameters'])) {
+                        $matching = true;
 
-                  foreach ($routeParameters as $index => $value) {
-                     if ('_' != substr($index, 0, 1)) {
-                        if (!isset($testedRoute['parameters'][$index]) || ($testedRoute['parameters'][$index] != $value && (0 < $testedRoute['parameters'][$index]) || !is_numeric($value) )) {
-                           $matching = false;
+                        foreach ($routeParameters as $index => $value) {
+                            if ('_' != substr($index, 0, 1)) {
+                                if (!isset($testedRoute['parameters'][$index]) || ($testedRoute['parameters'][$index] != $value && (0 < $testedRoute['parameters'][$index]) || !is_numeric($value))) {
+                                    $matching = false;
+                                }
+                            }
                         }
-                     }
-                  }
 
-                  if ($matching) {
-                     return true;
-                  }
-               }
-            } else {
-               return true;
+                        if ($matching) {
+                            return true;
+                        }
+                    }
+                } else {
+                    return true;
+                }
             }
-         }
-      }
+        }
 
-      return null;
-   }
+        return null;
+    }
 
 }
