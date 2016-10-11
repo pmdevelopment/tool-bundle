db4bf5d39def0fb7f6633d11b5e7569ff1b97a39
sjoder <sj@pmdevelopment.de>
2016-10-11 13:52:18 +0200

    New trait: user functions in services

---
+++ Framework/Traits/Services/HasUserServiceTrait.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sjoder
+ * Date: 11.10.2016
+ * Time: 13:23
+ */
+
+namespace PM\Bundle\ToolBundle\Framework\Traits\Services;
+
+use Symfony\Component\Security\Core\Authorization\AuthorizationChecker;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
+
+trait HasUserServiceTrait
+{
+    /**
+     * @var AuthorizationChecker
+     */
+    private $authorizationChecker;
+
+    /**
+     * @var TokenStorage
+     */
+    private $tokenStorage;
+
+    /**
+     * @return AuthorizationChecker
+     */
+    public function getAuthorizationChecker()
+    {
+        return $this->authorizationChecker;
+    }
+
+    /**
+     * @param AuthorizationChecker $authorizationChecker
+     *
+     * @return HasUserServiceTrait
+     */
+    public function setAuthorizationChecker($authorizationChecker)
+    {
+        $this->authorizationChecker = $authorizationChecker;
+
+        return $this;
+    }
+
+    /**
+     * @return TokenStorage
+     */
+    public function getTokenStorage()
+    {
+        return $this->tokenStorage;
+    }
+
+    /**
+     * @param TokenStorage $tokenStorage
+     *
+     * @return HasUserServiceTrait
+     */
+    public function setTokenStorage($tokenStorage)
+    {
+        $this->tokenStorage = $tokenStorage;
+
+        return $this;
+    }
+
+    /**
+     * Get User
+     *
+     * @return mixed
+     */
+    public function getUser()
+    {
+        return $this->getTokenStorage()->getToken()->getUser();
+    }
+}
