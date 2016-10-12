df2b245b596fed4ad82870aad1b725e781bf1ae8
sjoder <sj@pmdevelopment.de>
2016-10-12 16:02:32 +0200

    new feature: sluggable repository trait

---
+++ Framework/Traits/Repositories/SluggableRepositoryTrait.php
@@ -0,0 +1,30 @@
+<?php
+namespace PM\Bundle\ToolBundle\Framework\Traits\Repositories;
+
+/**
+ * Class SluggableRepositoryTrait
+ *
+ * @package PM\Bundle\ToolBundle\Framework\Traits\Services
+ */
+trait SluggableRepositoryTrait
+{
+
+    /**
+     * Find One By Slug
+     *
+     * @param string $slug
+     * @param string $fieldName
+     *
+     * @return mixed
+     */
+    public function findOneBySlug($slug, $fieldName = "slug")
+    {
+        return $this->findOneBy(
+            [
+                'slug'    => $slug,
+                'deleted' => false,
+            ]
+        );
+    }
+
+}
