0da87e03577de00499a04bdfdc60e5d39b3dcda9
sjoder <sj@pmdevelopment.de>
2016-11-01 16:06:57 +0100

    command utility to count running

---
+++ Framework/Utilities/CommandUtility.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sjoder
+ * Date: 01.11.2016
+ * Time: 15:56
+ */
+
+namespace PM\Bundle\ToolBundle\Framework\Utilities;
+
+/**
+ * Class CommandUtility
+ *
+ * @package PM\Bundle\ToolBundle\Framework\Utilities
+ */
+class CommandUtility
+{
+    /**
+     * Get Command Running count.
+     *
+     * Returns running php process count for given command name.
+     *
+     * @param string $name e.g. pm:bundle:command
+     *
+     * @return int
+     */
+    public static function getCommandRunningCount($name)
+    {
+        exec(sprintf("ps auxwww|grep %s|grep -v grep", $name), $output);
+
+        if (false === is_array($output)) {
+            return 1;
+        }
+
+        return count($output);
+    }
+
+}
