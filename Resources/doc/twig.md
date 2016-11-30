f32fed47d722747935b1f95f16fe04d1a4581320
sjoder <sj@pmdevelopment.de>
2016-11-30 15:00:44 +0100

    login choice tool

--- Resources/doc/twig.md
+++ Resources/doc/twig.md
@@ -37,3 +37,22 @@ Returns random Sha1 and MD5 Hash. If not empty the value will be hashed.
 ```
 
 Returns the HTML Image code to the given flag name (ISO 3166-1 alpha-2). FamFamFam Flag icons are used.
+
+
+## FOSUser
+
+```html
+{% if 'dev'==app.environment %}
+    <select class="form-control form-control-solid placeholder-no-fix form-group" name="_username">
+        {% for name in "PMCoreUserBundle:User"|fos_user_get_usernames %}
+            <option>{{ name }}</option>
+        {% endfor %}
+    </select>
+{% else %}
+    <input class="form-control form-control-solid placeholder-no-fix form-group" type="text" autocomplete="off" placeholder="Benutzername"
+           name="_username" required/>
+{% endif %}
+```
+
+Get choice selection for usernames in dev environment. Be sure your dev environment is not available online!
+
---
+++ Twig/FOSUserExtension.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sjoder
+ * Date: 30.11.2016
+ * Time: 14:50
+ */
+
+namespace PM\Bundle\ToolBundle\Twig;
+
+use PM\Bundle\ToolBundle\Framework\Traits\Services\HasDoctrineServiceTrait;
+use PM\Bundle\ToolBundle\Framework\Utilities\CollectionUtility;
+
+/**
+ * Class FOSUserExtension
+ *
+ * @package PM\Bundle\ToolBundle\Twig
+ */
+class FOSUserExtension extends \Twig_Extension
+{
+    use HasDoctrineServiceTrait;
+
+    /**
+     * @inheritDoc
+     */
+    public function getFilters()
+    {
+        return [
+            new \Twig_SimpleFilter('fos_user_get_usernames', [
+                $this,
+                'getUserNames',
+            ]),
+        ];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getName()
+    {
+        return "pm.twig.fos_user";
+    }
+
+    /**
+     * Get Usernames as array
+     *
+     * @param string $entity User Entity Class Name
+     *
+     * @return array
+     */
+    public function getUserNames($entity)
+    {
+        $users = $this->getDoctrine()->getRepository($entity)->findBy(
+            [
+                'enabled' => true,
+            ],
+            [
+                'username' => 'asc',
+            ]
+        );
+
+        return CollectionUtility::get('username', $users);
+    }
+}
