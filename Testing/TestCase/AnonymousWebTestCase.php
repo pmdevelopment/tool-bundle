5f7104491838b91925623efd69e8601ffcf43955
sjoder <sj@pmdevelopment.de>
2016-12-09 12:27:48 +0100

    webtest client follow redirects

---
+++ Testing/TestCase/AnonymousWebTestCase.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sjoder
+ * Date: 09.12.2016
+ * Time: 12:24
+ */
+
+namespace PM\Bundle\ToolBundle\Testing\TestCase;
+
+
+use Symfony\Bundle\FrameworkBundle\Client;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * Class AnonymousWebTestCase
+ *
+ * @package PM\Bundle\ToolBundle\Testing\TestCase
+ */
+class AnonymousWebTestCase extends WebTestCase
+{
+
+    /**
+     * Create Client
+     *
+     * followRedirects: true
+     *
+     * @return Client
+     */
+    public static function createClientFollowingRedirects()
+    {
+        $client = self::createClient();
+        $client->followRedirects(true);
+
+        return $client;
+    }
+
+}
