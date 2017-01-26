3db19900e08328ee6e1e3972c662707bbb21225a
Sven Joder <sj@pmdevelopment.de>
2017-01-26 10:37:22 +0100

    HTTP Method constants

---
+++ Constants/HttpMethod.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sjoder
+ * Date: 26.01.2017
+ * Time: 10:35
+ */
+
+namespace PM\Bundle\ToolBundle\Constants;
+
+/**
+ * Class HttpMethod
+ *
+ * @package PM\Bundle\ToolBundle\Constants
+ */
+class HttpMethod
+{
+    const GET = 'GET';
+    const POST = 'POST';
+    const PUT = 'PUT';
+    const DELETE = 'DELETE';
+    const TRACE = 'TRACE';
+    const CONNECT = 'CONNECT';
+    const HEAD = 'HEAD';
+    const OPTIONS = 'OPTIONS';
+
+}
