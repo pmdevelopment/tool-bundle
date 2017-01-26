e9ea2fb00f0b13747dbd79366c3a6e8eb66f9efc
Sven Joder <sj@pmdevelopment.de>
2017-01-26 11:23:27 +0100

    aria-hidden for icons, minor enhancements

--- Twig/IconExtension.php
+++ Twig/IconExtension.php
@@ -1,25 +1,23 @@
 <?php
-/**
- * This file is part of BcBootstrapBundle.
- *
- * (c) 2012-2013 by Florian Eckerstorfer
- */
-
 namespace PM\Bundle\ToolBundle\Twig;
 
 use Twig_Extension;
 use Twig_SimpleFilter;
 
 /**
- * BootstrapIconExtension
+ * IconExtension
  *
- * @category   TwigExtension
- * @package    BcBootstrapBundle
- * @subpackage Twig
+ *
  * @author     Florian Eckerstorfer <florian@eckerstorfer.co>
+ * @author     Sven Joder <sj@pmdevelopment.de>
+ *
  * @copyright  2012-2013 Florian Eckerstorfer
+ * @copyright  2013-2017 Sven Joder
+ *
  * @license    http://opensource.org/licenses/MIT The MIT License
+ *
  * @link       http://bootstrap.braincrafted.com Bootstrap for Symfony2
+ * @link       http://www.pmdevelopment.de
  */
 class IconExtension extends Twig_Extension
 {
@@ -29,14 +27,30 @@ class IconExtension extends Twig_Extension
     public function getFilters()
     {
         return [
-            new Twig_SimpleFilter("parse_icons", [
-                $this,
-                "parseIconsFilter"
-            ], ["is_safe" => ["html"]]),
-            new Twig_SimpleFilter("icon", [
-                $this,
-                "iconFilter"
-            ], ["is_safe" => ["html"]])
+            new Twig_SimpleFilter(
+                'parse_icons',
+                [
+                    $this,
+                    'parseIconsFilter',
+                ],
+                [
+                    'is_safe' => [
+                        'html',
+                    ],
+                ]
+            ),
+            new Twig_SimpleFilter(
+                'icon',
+                [
+                    $this,
+                    'iconFilter',
+                ],
+                [
+                    'is_safe' => [
+                        'html',
+                    ],
+                ]
+            )
         ];
     }
 
@@ -44,7 +58,7 @@ class IconExtension extends Twig_Extension
      * Parses the given string and replaces all occurrences of .icon-[name] with the corresponding icon.
      *
      * @param string $text  The text to parse
-     * @param string $color The color of the icon; can be "black" or "white"; defaults to "black"
+     * @param string $color The color of the icon; can be 'black' or 'white'; defaults to 'black'
      *
      * @return string The HTML code with the icons
      */
@@ -65,13 +79,13 @@ class IconExtension extends Twig_Extension
      * Returns the HTML code for the given icon.
      *
      * @param string $icon  The name of the icon
-     * @param string $color The color of the icon; can be "black" or "white"; defaults to "black"
+     * @param string $color The color of the icon; can be 'black' or 'white'; defaults to 'black'
      *
      * @return string The HTML code for the icon
      */
     public function iconFilter($icon, $color = 'black')
     {
-        return sprintf('<i class="fa %sfa-%s"></i>', $color == 'white' ? 'icon-white ' : '', $icon);
+        return sprintf('<i class="fa %sfa-%s" aria-hidden="true"></i>', $color == 'white' ? 'icon-white ' : '', $icon);
     }
 
     /**
@@ -79,7 +93,7 @@ class IconExtension extends Twig_Extension
      */
     public function getName()
     {
-        return 'pm_icon_extension';
+        return 'pm.twig.icon';
     }
 }
 
