dd40c6a90a30ef9dacd454b4da08f4033bbd97d3
Sven Joder <sj@pmdevelopment.de>
2017-05-18 11:46:06 +0200

    event dispatcher as trait

---
+++ Framework/Traits/Services/HasEventDispatcherServiceTrait.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sjoder
+ * Date: 18.05.2017
+ * Time: 11:44
+ */
+
+namespace PM\Bundle\ToolBundle\Framework\Traits\Services;
+
+use Symfony\Component\EventDispatcher\EventDispatcher;
+
+/**
+ * Class HasEventDispatcherServiceTrait
+ *
+ * @package PM\Bundle\ToolBundle\Framework\Traits\Services
+ */
+trait HasEventDispatcherServiceTrait
+{
+    /**
+     * @var EventDispatcher
+     */
+    private $eventDispatcher;
+
+    /**
+     * @return EventDispatcher
+     */
+    public function getEventDispatcher()
+    {
+        if (null === $this->eventDispatcher) {
+            throw new \RuntimeException('EventDispatcher not found. Setter not called?');
+        }
+
+        return $this->eventDispatcher;
+    }
+
+    /**
+     * @param EventDispatcher $eventDispatcher
+     *
+     * @return HasEventDispatcherServiceTrait
+     */
+    public function setEventDispatcher($eventDispatcher)
+    {
+        $this->eventDispatcher = $eventDispatcher;
+
+        return $this;
+    }
+
+}
