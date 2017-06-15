dfddb21b4d5a53320c24507921ca372952b03d34
Sven Joder <sj@pmdevelopment.de>
2017-06-15 14:33:38 +0200

    missing phpdoc

--- PMToolBundle.php
+++ PMToolBundle.php
@@ -4,6 +4,11 @@ namespace PM\Bundle\ToolBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * Class PMToolBundle
+ *
+ * @package PM\Bundle\ToolBundle
+ */
 class PMToolBundle extends Bundle
 {
 }
