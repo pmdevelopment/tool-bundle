2497767b73e59bc57cd618572593e3842e725d1f
Sven Joder <sj@pmdevelopment.de>
2018-07-27 11:20:02 +0200

    doctrine constants

---
+++ Constants/Doctrine.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sjoder
+ * Date: 27.07.2018
+ * Time: 11:02
+ */
+
+namespace PM\Bundle\ToolBundle\Constants;
+
+
+/**
+ * Class Doctrine
+ *
+ * @package PM\Bundle\ToolBundle\Constants
+ */
+class Doctrine
+{
+    const DIRECTION_ASC = 'asc';
+    const DIRECTION_DESC = 'desc';
+}
